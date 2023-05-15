b5d4cfe3c192c6e8913828af55d70c5c07e3d71d
Zach <zachlandis91@gmail.com>
2023-05-15 14:01:18 -0600

    create get request on events route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/events" do
+    events = Event.all 
+    events.to_json
   end
+
+  # post "/events/:id" do
+  #   event = Event.create(
+  #     event_name: params[:event_name]
+  #   )
+
+
 
 end
