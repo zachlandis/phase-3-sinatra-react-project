373b8b6a72af303561975618429696b347ea46f9
Zach <zachlandis91@gmail.com>
2023-05-15 13:41:16 -0600

    reseed with more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,18 @@ puts "🌱 Seeding data..."
         event_date: Faker::Date.in_date_period
     )
 
-    attendee = Attendee.create(
-        first_name: Faker::Name.unique.first_name,
-        last_name: Faker::Name.unique.last_name,
-        email: Faker::Internet.email
-    )
+    10.times do
+        attendee = Attendee.create(
+            first_name: Faker::Name.unique.first_name,
+            last_name: Faker::Name.unique.last_name,
+            email: Faker::Internet.email
+        )
 
-    ticket = Ticket.create(
-        ticket_number: rand(51246..98365),
-        event_id: event.id,
-        attendee_id: attendee.id
-    )
+        Ticket.create(
+            ticket_number: rand(51246..98365),
+            event_id: event.id,
+            attendee_id: attendee.id
+        )
+    end
 end
 puts "🌱 Done seeding!"
