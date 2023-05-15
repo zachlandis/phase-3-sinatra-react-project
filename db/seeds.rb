745903c6d42032ac3e7b4efa9399492b577e8c82
Zach <zachlandis91@gmail.com>
2023-05-15 12:59:53 -0600

    Create attendee seed and install Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+10.times do
+    attendee = Attendee.create(
+        first_name: Faker::Name.unique.first_name
+        last_name: Faker::Name.unique.last_name
+        email: Faker::Internet.email
+)
+
+
 
 puts "✅ Done seeding!"
