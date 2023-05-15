e18679e2ff18d8b0f3c42ab90ac158f067604b94
Zach <zachlandis91@gmail.com>
2023-05-15 13:05:12 -0600

    Finish seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 puts "🌱 Seeding spices..."
 
+3.times do 
+    event = Event.create(
+        event_name: Faker::Lorem.word
+        headliner: Faker::Lorem.word
+        event_venue: Faker::Lorem.word
+        capacity: rand(200..20000)
+        event_date: Faker::Date.in_date_period
+    )
+
 10.times do
     attendee = Attendee.create(
         first_name: Faker::Name.unique.first_name
@@ -7,6 +16,20 @@ puts "🌱 Seeding spices..."
         email: Faker::Internet.email
 )
 
+10.times do
+    ticket = Ticket.create(
+        ticket_number: rand(51246..98365)
+        event_id: event.id
+        attendee_id: attendee.id
+    )
+
 
 
 puts "✅ Done seeding!"
+
+
+t.string :headliner 
+t.string :event_venue
+t.integer :capacity 
+t.datetime :event_date
+t.string :event_updates
